eeb103163eec0d568a5584406fd14c041d3fc60e
vhincapie <vivianahincapie1610@gmail.com>
2025-05-19 18:45:50 -0500

    Delete src directory

--- src/main/java/co/edu/unbosque/gestiontarjetas/service/interfaces/IClienteService.java
+++
@@ -1,9 +0,0 @@
-package co.edu.unbosque.gestiontarjetas.service.interfaces;
-
-import co.edu.unbosque.gestiontarjetas.model.DTO.ClienteDTO;
-import java.util.List;
-
-public interface IClienteService {
-
-    List<ClienteDTO> listarClientes();
-}
